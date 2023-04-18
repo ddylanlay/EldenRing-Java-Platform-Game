657febe9a10033525b3825e13d552a7b384392e2
ddylanlay <109592570+ddylanlay@users.noreply.github.com>
2023-04-18 21:30:12 +1000

    adding runes to trader

--- src/game/Runes.java
+++ src/game/Runes.java
@@ -4,4 +4,7 @@ public interface Runes {
     public boolean removeRunes(int totalRunes, int runes) {
         return false;
     }
+    public boolean addRunes(int totalRunes, int runes) {
+        return false;
+    }
 }
