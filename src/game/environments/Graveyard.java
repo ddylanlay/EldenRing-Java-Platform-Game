44d1cc0065491ca6eaebe3ef8c568b1a0fb2c002
jtra0050 <jtra0050@student.monash.edu>
2023-04-18 18:54:34 +1000

    18/04/2023 Edited the environments classes
    Edited the classes Graveyard, GustOfWind and PuddleOfWater to have a chance of spawning an enemy every turn.

--- src/game/environments/Graveyard.java
+++ src/game/environments/Graveyard.java
@@ -1,4 +1,21 @@
 package game.environments;
 
-public class Graveyard {
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.utils.RandomNumberGenerator;
+
+public class Graveyard extends Ground {
+
+    public Graveyard(){
+        super('n');
+    }
+    public void tick(Location spawnLocation){
+        if(!spawnLocation.containsAnActor()){
+            if(RandomNumberGenerator.getRandomInt(100)<=27){
+                // create actor heavy skeletal swordsman
+            }
+        }
+
+
+    }
 }
--- src/game/environments/GustOfWind.java
+++ src/game/environments/GustOfWind.java
@@ -1,4 +1,21 @@
 package game.environments;
 
-public class GustOfWind {
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.utils.RandomNumberGenerator;
+
+public class GustOfWind extends Ground {
+
+    public GustOfWind(){
+        super('&');
+    }
+    public void tick(Location spawnLocation){
+        if(!spawnLocation.containsAnActor()){
+            if(RandomNumberGenerator.getRandomInt(100)<=33){
+                // create actor lone wolf
+            }
+        }
+
+
+    }
 }
