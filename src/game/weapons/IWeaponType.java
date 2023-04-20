2f9a65f535aabddd8ec398d4cbd41b757c179751
Arosh Heenkenda <ahee0011@student.monash.edu>
2023-04-21 09:14:35 +1000

    Add enum values for WeaponType, get rid of IWeaponType interface as implementation won't work.

--- src/game/weapons/IWeaponType.java
+++
@@ -1,4 +0,0 @@
-package game.weapons;
-
-public interface IWeaponType {
-}
--- src/game/weapons/WeaponType.java
+++ src/game/weapons/WeaponType.java
@@ -1,4 +1,9 @@
 package game.weapons;
 
 public enum WeaponType {
+
+    KATANA,
+    DAGGER,
+    HAMMER,
+    CURVED_SWORD
 }
