5fa5afd2a4a20cfcf88921e7a439b20df7f0b7ea
ddylanlay <109592570+ddylanlay@users.noreply.github.com>
2023-04-25 18:14:22 +1000

    [ADD] sell action with Kale works now

--- src/game/actionsgame/PurchaseAction.java
+++ src/game/actionsgame/PurchaseAction.java
@@ -3,6 +3,7 @@ package game.actionsgame;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.Player;
 import game.trading.PurchasableItem;
 
@@ -21,6 +22,7 @@ public class PurchaseAction extends Action {
         }
         else{
             player.removeRunes(weapon.getPurchasePrice());
+            player.addWeaponToInventory((WeaponItem) weapon);
             return menuDescription(player);
         }
 
