a16dfca76d5bad6ba202b6683a32ecb15eedfb3f
Arosh Heenkenda <ahee0011@student.monash.edu>
2023-05-08 12:46:07 +1000

    Create the consume golden rune action.

---
+++ src/game/actionsgame/ConsumeRuneAction.java
@@ -0,0 +1,28 @@
+package game.actionsgame;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+/**
+ * Consume action for the Golden Runes
+ *
+ * Created by:
+ * @author Arosh Heenkenda
+ *
+ * Modified by:
+ *
+ */
+public class ConsumeRuneAction extends Action {
+
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
