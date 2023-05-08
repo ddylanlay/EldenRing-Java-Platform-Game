4c5dd93e34698331ba2ce80d88e9c626f977f754
Arosh Heenkenda <ahee0011@student.monash.edu>
2023-05-08 10:58:48 +1000

    Create Golden Runes class.

---
+++ src/game/items/GoldenRunes.java
@@ -0,0 +1,33 @@
+package game.items;
+
+import edu.monash.fit2099.engine.actors.Actor;
+
+/**
+ * Golden Runes Item Class
+ *
+ * Created by:
+ * @author Arosh Heenkenda
+ *
+ * Modified by:
+ *
+ */
+public class GoldenRunes extends ConsumeableItem {
+
+    /***
+     * Constructor.
+     *
+     */
+    public GoldenRunes() {
+        super("Golden Runes", '*', true);
+    }
+
+    @Override
+    public void consume() {
+
+    }
+
+    @Override
+    public String consume(Actor actor, int healthAmount) {
+        return null;
+    }
+}
