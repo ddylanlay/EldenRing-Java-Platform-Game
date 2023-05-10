5a2534415a7033f4ad4d48f39d91e596b71df0cc
jtra0050 <jtra0050@student.monash.edu>
2023-05-10 20:06:30 +1000

    10/05/2023 Added new class astrologer

---
+++ src/game/combatclass/Astrologer.java
@@ -0,0 +1,10 @@
+package game.combatclass;
+
+import game.weapons.GreatKnife;
+
+public class Astrologer extends CombatClass{
+    public Astrologer(){
+        super("Astrologer", 'a', 396, new GreatKnife());
+
+    }
+}
