33d6685073e0493eccfe2dc57f609018209e9d91
ddylanlay <109592570+ddylanlay@users.noreply.github.com>
2023-05-20 14:04:33 +1000

    [REMOVE] comments in CombatClass

--- src/game/combatclass/CombatClass.java
+++ src/game/combatclass/CombatClass.java
@@ -97,46 +97,4 @@ public abstract class CombatClass {
     public String toString(){ return className; }
 
 
-
-//    public CombatClass(String name, char displayChar, int hitPoints){
-//        super(name, displayChar, hitPoints);
-//    }
-//    public String toString(){
-//        return "the One";
-//    }
-//
-//
-//    @Override
-//    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-//        ActionList actions = super.allowableActions(otherActor, direction, map);
-//        actions.add(new SelectCombatClassAction(new Bandit()));
-//        actions.add(new SelectCombatClassAction(new Samurai()));
-//        actions.add(new SelectCombatClassAction(new Wretch()));
-//        return actions;
-//    }
-//
-//    @Override
-//    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-//        // Handle multi-turn Actions
-//        if (lastAction.getNextAction() != null)
-//            return lastAction.getNextAction();
-//        System.out.println("Select your role:");
-//        // return/print the console menu
-//        return combatClassMenu.showMenu(this, actions, display);
-//    }
-
-//    /**
-//     * Getter for maxHitPoints
-//     *
-//     * @return an int value for the max hit points of a combat class.
-//     */
-//    public int getMaxHitPoints() { return maxHitPoints; }
-//
-//    /**
-//     * Getter for classWeapon
-//     *
-//     * @return an object of WeaponItem for the weapon of a combat class
-//     */
-//    public WeaponItem getClassWeapon() { return classWeapon;}
-
 }
